b507d8513af174b726529462e2cdf71d2e7a05d1
TOK_김영주 <feel2174@gmail.com>
2025-11-05 15:34:01 +0900

    gtm

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,22 +21,18 @@ class MyDocument extends Document {
 
   googleTagManagerHeadScript() {
     return {
-      __html: `
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WQBJW8DZ');</script>
+})(window,document,'script','dataLayer','GTM-WQBJW8DZ');
     `,
     }
   }
   googleTagManagerBodyScript() {
     return {
-      __html: `
-    <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQBJW8DZ"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQBJW8DZ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
     `,
     }
   }
